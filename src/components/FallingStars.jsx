9c9bc90ed224cf8a340cb1ce9596a80eefb52939
atulj10 <atulanandj10@gmail.com>
2026-09-10 22:08:01 +0530

    neon lights added in the background

--- src/components/FallingStars.jsx
+++ src/components/FallingStars.jsx
@@ -1,6 +1,10 @@
 export default function FallingStars() {
   return (
     <div className="stars-layer">
+      <div className="neon-orbit">
+        <div className="neon-ball neon-ball--pink"></div>
+        <div className="neon-ball neon-ball--blue"></div>
+      </div>
       <div id="stars"></div>
       <div id="stars2"></div>
       <div id="stars3"></div>
